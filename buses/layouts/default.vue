ca7678764e39ad1e37819ff5a0e921d7d1635e4a
osocaramelosofer <osocaramelosofer@hotmail.com>
2023-03-10 13:36:41 -0600

    change for better class names

---
+++ buses/layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <nav>
+      <nuxt-link to="/">Home</nuxt-link> |
+      <nuxt-link to="/blog">Blog</nuxt-link>
+    </nav>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style>
+main{
+  background-image: url("@/assets/bg2.jpg");
+  height: 100vh;
+  width: 100%;
+
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+}
+</style>
--- buses/pages/index.vue
+++ buses/pages/index.vue
@@ -1,47 +1,34 @@
 <template>
-  <main class="p-7">
-    <div class="wrapper">
-      <div class="container one">one</div>
-      <div class="container two">two</div>
-      <div class="container three">three</div>
-      <div class="container four">four</div>
-      <div class="container five">five</div>
+    <div class="grid-container">
+      <div class="item one">one</div>
+      <div class="item two">two</div>
+      <div class="item three">three</div>
+      <div class="item four">four</div>
+      <div class="item five">five</div>
     </div>
-  </main>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-main{
-  background-image: url("@/assets/bg2.jpg");
-  height: 100vh;
-  width:100vw;
 
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center center;
-}
-.wrapper{
+.grid-container {
   display: grid;
   width: 100%;
   height: 100%;
   grid-template-rows: repeat(4, 100px);
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
 
-/*
+  /*
   grid-auto-rows: minmax(100px, auto); Esto se para cuando se defina automaticamente una row extra ponerle el tamano establecido por defecto  */
   /* grid-auto-columns: minmax(100px, auto); */
   grid-auto-rows: 100px;
   grid-gap: 25px;
   align-content: center;
-
 }
-.container{
+.item {
   border-radius: 10px;
 
   background: rgba(255, 255, 255, 0.41);
@@ -50,36 +37,10 @@ main{
   backdrop-filter: blur(13.2px);
   -webkit-backdrop-filter: blur(13.2px);
 }
-.one {
-  /* grid-column: 1 / 3;
-  grid-row: 1/3; */
 
-
-
-}
-/*
-.two {
-  grid-column: 3 / 4;
-  grid-row: 1 / 3;
-
-} */
-/* 
-.three {
-  grid-column: 1 / 2;
-  grid-row: 3/5;
-
-}
-.four {
-  grid-column: 2;
-  grid-row: 3/5;
-}
-.five {
-  grid-column: 3/4;
-  grid-row: 3/5;
-} */
 /* 320px, 768px, and 1200px */
-@media only screen and (min-width: 1200px){
-  .wrapper{
+@media only screen and (min-width: 1200px) {
+  .grid-container {
     grid-template-columns: repeat(3, 1fr);
     grid-template-rows: repeat(4, 200px);
   }
@@ -88,10 +49,9 @@ main{
     grid-column: span 2;
     grid-row: 1/3;
   }
-  .two{
+  .two {
     grid-column: span 1;
     grid-row: span 2;
   }
-
 }
 </style>
