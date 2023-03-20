3a64fdebab1ab591cffbec2b2ddf3e29c0d79beb
osocaramelosofer <osocaramelosofer@hotmail.com>
2023-03-19 20:33:19 -0600

    add vuetify

--- buses/nuxt.config.ts
+++ buses/nuxt.config.ts
@@ -1,8 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
   modules: [
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+  css: ['vuetify/lib/styles/main.sass'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
 
 })
